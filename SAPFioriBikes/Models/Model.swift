90e863e15b206245ba44ee1ba67feab562f6714d
Alex Takahashi <alex.takahashi@sap.com>
2018-11-14 22:39:16 -0800

    shows point annotations

--- SAPFioriBikes/Models/Model.swift
+++ SAPFioriBikes/Models/Model.swift
@@ -7,35 +7,36 @@
 //
 
 import Foundation
+import MapKit
 import UIKit
 
-struct APIRequest: Decodable {
+struct StationInformationRequest: Decodable {
     let last_updated: CGFloat?
     let ttl: Int?
-    let data: Data?
+    let data: StationInformationData?
 }
 
-struct Data: Decodable {
-    let stations: [Station?]?
+struct StationInformationData: Decodable {
+    let stations: [StationInformation?]?
 }
 
-struct Station: Decodable {
+struct StationInformation: Decodable {
     let station_id: String?
     let external_id: String?
     let name: String?
     let short_name: String?
-    let lat: CGFloat?
-    let lon: CGFloat?
+    let lat: CLLocationDegrees?
+    let lon: CLLocationDegrees?
     let region_id: Int?
     let rental_methods: [String?]?
     let capacity: Int?
     let rental_url: String?
     let eightd_has_key_dispenser: Bool?
-    let eightd_station_services: [StationServices?]?
+    let eightd_station_services: [StationInformationServices?]?
     let has_kiosk: Bool?
 }
 
-struct StationServices: Decodable {
+struct StationInformationServices: Decodable {
     let id: String?
     let service_type: String?
     let bikes_availability: String?
@@ -44,4 +45,64 @@ struct StationServices: Decodable {
     let description: String?
     let schedule_description: String?
     let link_for_more_info: String?
+}
+
+struct StationStatusRequest: Decodable {
+    let last_updated: CGFloat?
+    let ttl: Int?
+    let data: StationStatusData?
+}
+
+struct StationStatusData: Decodable {
+    let stations: [StationStatus?]?
+}
+
+struct StationStatus: Decodable {
+    let station_id: String?
+    let num_bikes_available: Int?
+    let num_ebikes_available: Int?
+    let num_bikes_disabled: Int?
+    let num_docks_available: Int?
+    let num_docks_disabled: Int?
+    let is_installed: Int?
+    let is_renting: Int?
+    let is_returning: Int?
+    let last_reported: CGFloat?
+    let eightd_has_available_keys: Bool?
+    let eightd_active_station_services: [StationStatusServices?]?
+}
+
+struct StationStatusServices: Decodable {
+    let id: String?
+}
+
+class SAPFioriBikeStation: NSObject, MKAnnotation {
+    var information: StationInformation? = nil
+    var status: StationStatus? = nil
+    var coordinate: CLLocationCoordinate2D {
+        get {
+            guard let information = information, let lat = information.lat, let lon = information.lon else { return CLLocationCoordinate2D(latitude: 0, longitude: 0)}
+            return CLLocationCoordinate2D(latitude: lat, longitude: lon)
+        }
+    }
+    var title: String? {
+        get {
+            return  "abc"
+        }
+    }
+//    var _pointAnnotation: MKPointAnnotation? = nil
+//
+//    var pointAnnotation: MKPointAnnotation? {
+//        get {
+//            guard let coordinate = coordinate else { return nil}
+//            if _pointAnnotation == nil {
+//                _pointAnnotation = MKPointAnnotation()
+//                _pointAnnotation!.coordinate = coordinate
+//            }
+//            return _pointAnnotation
+//        }
+//        set {
+//            _pointAnnotation = newValue
+//        }
+//    }
 }
--- SAPFioriBikes/ViewController.swift
+++ SAPFioriBikes/ViewController.swift
@@ -7,28 +7,111 @@
 //
 
 import UIKit
+import MapKit
 
 class ViewController: UIViewController {
     
-    var model: [Station] = []
+    var stationInformationModel: [StationInformation] = []
+    var stationStatusModel: [StationStatus] = []
+    var stationDictionary: [String: SAPFioriBikeStation] = [:]
+    var mapView: MKMapView = MKMapView()
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        guard let jsonURLString: URL = URL(string: "https://gbfs.fordgobike.com/gbfs/en/station_information.json")else { return }
-        URLSession.shared.dataTask(with: jsonURLString) { [weak self] (data, response, err) in
+        guard let stationInformationURL: URL = URL(string: "https://gbfs.fordgobike.com/gbfs/en/station_information.json")else { return }
+        URLSession.shared.dataTask(with: stationInformationURL) { [weak self] (data, response, err) in
             guard let data = data else { return }
             do {
                 guard let strongSelf = self else { return }
-                let request = try JSONDecoder().decode(APIRequest.self, from: data)
+                let request = try JSONDecoder().decode(StationInformationRequest.self, from: data)
                 guard let stations = request.data?.stations else { return }
-                strongSelf.model = stations.compactMap({return $0})
-                for station in strongSelf.model {
-                    print(station.name)
+                strongSelf.stationInformationModel = stations.compactMap({return $0})
+                for station in strongSelf.stationInformationModel {
+                    strongSelf.merge(station)
                 }
+                print(strongSelf.stationDictionary)
             } catch let jsonError {
                 print("❌ \(jsonError)")
             }
         }.resume()
+        
+        guard let stationStatusURL: URL = URL(string: "https://gbfs.fordgobike.com/gbfs/en/station_status.json")else { return }
+        URLSession.shared.dataTask(with: stationStatusURL) { [weak self] (data, response, err) in
+            guard let data = data else { return }
+            do {
+                guard let strongSelf = self else { return }
+                let request = try JSONDecoder().decode(StationStatusRequest.self, from: data)
+                guard let stations = request.data?.stations else { return }
+                strongSelf.stationStatusModel = stations.compactMap({return $0})
+                for station in strongSelf.stationStatusModel {
+                    strongSelf.merge(station)
+                }
+                print(strongSelf.stationDictionary)
+            } catch let jsonError {
+                print("❌ \(jsonError)")
+            }
+        }.resume()
+        
+        mapView.delegate = self
+        self.view.addSubview(mapView)
+        mapView.translatesAutoresizingMaskIntoConstraints = false
+        mapView.leadingAnchor.constraint(equalTo: self.view.leadingAnchor).isActive = true
+        mapView.trailingAnchor.constraint(equalTo: self.view.trailingAnchor).isActive = true
+        mapView.topAnchor.constraint(equalTo: self.view.topAnchor).isActive = true
+        mapView.bottomAnchor.constraint(equalTo: self.view.bottomAnchor).isActive = true
+    }
+    
+    var dummyAnnotation = MKPointAnnotation()
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        let centerCoordinate = CLLocationCoordinate2D(latitude: 37.7749, longitude: -122.4194)
+        let span = MKCoordinateSpan(latitudeDelta: 0.2, longitudeDelta: 0.2)
+        let region = MKCoordinateRegion(center: centerCoordinate, span: span)
+        mapView.setRegion(region, animated: true)
+        dummyAnnotation.coordinate = centerCoordinate
+        mapView.addAnnotation(dummyAnnotation)
+    }
+    
+    internal func merge(_ status: StationStatus) {
+        guard let stationID = status.station_id else { return }
+        guard stationDictionary.contains(where: { return $0.key == stationID }) else {
+            let newFioriStation = SAPFioriBikeStation()
+            newFioriStation.status = status
+            stationDictionary[stationID] = newFioriStation
+            return
+        }
+        guard let cachedFioriStation = stationDictionary[stationID] else { return }
+        cachedFioriStation.status = status
+        mapView.addAnnotation(cachedFioriStation)
+        print("💥 added: \(cachedFioriStation.coordinate)")
+    }
+    
+    internal func merge(_ information: StationInformation) {
+        guard let stationID = information.station_id else { return }
+        guard stationDictionary.contains(where: { return $0.key == stationID }) else {
+            let newFioriStation = SAPFioriBikeStation()
+            newFioriStation.information = information
+            stationDictionary[stationID] = newFioriStation
+            return
+        }
+        guard let cachedFioriStation = stationDictionary[stationID] else { return }
+        cachedFioriStation.information = information
+        mapView.addAnnotation(cachedFioriStation)
+        print("💥 added: \(cachedFioriStation.coordinate)")
     }
 }
 
+extension ViewController: MKMapViewDelegate {
+    
+//    func mapView(_ mapView: MKMapView, viewFor annotation: MKAnnotation) -> MKAnnotationView? {
+//        return MKAnnotationView(annotation: annotation, reuseIdentifier: "abc")
+//    }
+    
+//    func mapView(_ mapView: MKMapView, didAdd views: [MKAnnotationView]) {
+//        for view in views {
+//            print("🍏 \(view.annotation?.coordinate)")
+//        }
+//    }
+    
+}
