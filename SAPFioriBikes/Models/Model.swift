1566f293814c2ca838d4f053496225ad905f0ecb
Alex Takahashi <alex.takahashi@sap.com>
2018-11-15 22:02:57 -0800

    annotations appear

--- SAPFioriBikes/Models/Model.swift
+++ SAPFioriBikes/Models/Model.swift
@@ -110,6 +110,12 @@ class SAPFioriBikeStation: NSObject, FUIAnnotation {
             return status?.num_ebikes_available ?? 0
         }
     }
+    
+    var numDocsAvailable: Int {
+        get {
+            return status?.num_docks_available ?? 0
+        }
+    }
 }
 
 class BikeClusterAnnotation: MKAnnotationView {
@@ -130,37 +136,40 @@ class BikeClusterAnnotation: MKAnnotationView {
         
         if let bikeStationAnnotation = annotation as? SAPFioriBikeStation {
             let totalBikes = bikeStationAnnotation.numBikes + bikeStationAnnotation.numEBikes
-            image = drawRatio(bikeStationAnnotation.numBikes, to: totalBikes, fractionColor: UIColor.red, wholeColor: UIColor.blue)
+            image = drawRatio(bikeStationAnnotation.numBikes, to: totalBikes, fractionColor: UIColor(hexString: "80B030"), wholeColor: UIColor(hexString: "002E6D"))
+            displayPriority = .defaultLow
         }
     }
     
     private func drawRatio(_ fraction: Int, to whole: Int, fractionColor: UIColor?, wholeColor: UIColor?) -> UIImage {
-        let renderer = UIGraphicsImageRenderer(size: CGSize(width: 40, height: 40))
+        let squarelength: CGFloat = 35
+        let renderer = UIGraphicsImageRenderer(size: CGSize(width: squarelength, height: squarelength))
         return renderer.image { _ in
             // Fill full circle with wholeColor
             wholeColor?.setFill()
-            UIBezierPath(ovalIn: CGRect(x: 0, y: 0, width: 40, height: 40)).fill()
+            UIBezierPath(ovalIn: CGRect(x: 0, y: 0, width: squarelength, height: squarelength)).fill()
             
             // Fill pie with fractionColor
             fractionColor?.setFill()
             let piePath = UIBezierPath()
-            piePath.addArc(withCenter: CGPoint(x: 20, y: 20), radius: 20,
+            piePath.addArc(withCenter: CGPoint(x: squarelength/2, y: squarelength/2), radius: squarelength/2,
                            startAngle: 0, endAngle: (CGFloat.pi * 2.0 * CGFloat(fraction)) / CGFloat(whole),
                            clockwise: true)
-            piePath.addLine(to: CGPoint(x: 20, y: 20))
+            piePath.addLine(to: CGPoint(x: squarelength/2, y: squarelength/2))
             piePath.close()
             piePath.fill()
             
             // Fill inner circle with white color
             UIColor.white.setFill()
-            UIBezierPath(ovalIn: CGRect(x: 8, y: 8, width: 24, height: 24)).fill()
+            UIBezierPath(arcCenter: CGPoint(x: squarelength/2, y: squarelength/2), radius: squarelength/4, startAngle: 0, endAngle: 360, clockwise: true).fill()
+//            UIBezierPath(ovalIn: CGRect(x: 8, y: 8, width: squarelength/2, height: squarelength/2)).fill()
             
             // Finally draw count text vertically and horizontally centered
-            let attributes = [ NSAttributedString.Key.foregroundColor: UIColor.black,
-                               NSAttributedString.Key.font: UIFont.boldSystemFont(ofSize: 20)]
+            let attributes = [ NSAttributedString.Key.foregroundColor: UIColor.black/*,
+                               NSAttributedString.Key.font: UIFont.boldSystemFont(ofSize: 20)*/]
             let text = "\(whole)"
             let size = text.size(withAttributes: attributes)
-            let rect = CGRect(x: 20 - size.width / 2, y: 20 - size.height / 2, width: size.width, height: size.height)
+            let rect = CGRect(x: squarelength/2 - size.width / 2, y: squarelength/2 - size.height / 2, width: size.width, height: size.height)
             text.draw(in: rect, withAttributes: attributes)
         }
     }
